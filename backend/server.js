6baf38cc92fbf990f5edcd225865cda4b617664a
Edwyn <edwyn.saunders@outlook.com>
2022-03-12 05:00:23 -0500

    Server start, set type to module

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+// NASA API Stuff Server
+
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const OURTESTVAR = process.env.Test_Var;
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+console.log (OURTESTVAR);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
