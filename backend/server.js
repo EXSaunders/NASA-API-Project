1248bd4405c81496104c5a04744626b56c6f58e7
Edwyn <edwyn.saunders@outlook.com>
2022-03-12 05:22:15 -0500

    Added in nodemon dev dependency

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const OURTESTVAR = process.env.Test_Var;
+const OURTESTVAR = process.env.Test_Var || 'No Test Var';
 const PORT = process.env.PORT || 3000;
 const app = express();
 
